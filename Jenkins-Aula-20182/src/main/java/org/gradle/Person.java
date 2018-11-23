384b1a1b87ab28200d040e362426e872960baf1f
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2018-11-23 10:34:09 -0300

    First commit!

---
+++ Jenkins-Aula-20182/src/main/java/org/gradle/Person.java
@@ -0,0 +1,17 @@
+package org.gradle;
+
+import org.apache.commons.collections.list.GrowthList;
+
+public class Person {
+    private final String name;
+
+    public Person(String name) {
+        this.name = name;
+        new GrowthList();
+    }
+
+    public String getName() {
+        return "XXX";
+    	//return name;
+    }
+}
